a8eccfbc33d93319f4d053e7c4e0bd2e0937a37a
Subhalaxmi Behera <108182370+CanGems@users.noreply.github.com>
2023-03-13 20:26:19 +0530

    comment

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -6,7 +6,7 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-@Component
+@Component  //from here tracker command start
 public class Tracker {
     @Autowired
     private List<TimeEntry> entries;
